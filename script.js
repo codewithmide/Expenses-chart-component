4bbbaaa20abbfd9819081a028ed0907318965675
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-22 23:24:15 +0100

    continue tommorow

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-import { Chart } from "chart.js";
+var ctx = document.getElementById("my_chart").getContext("2d");
 
-const chartElement = document.getElementById('chart');
-const config = {
-    type: 'bar',
-    data: {label: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-    dataset: [{label: 'Expenses', data: [2, 5, 3, 2, 1, 2, 5]}] }
-}
-
-const barChart = new Chart(chartElement, config);
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                "mon",
+                "tue",
+                "wed",
+                "thu",
+                "fri",
+                "sat",
+                "sun"
+            ],
+            datasets: [{
+                data: [
+                    17.5,
+                    34.91,
+                    52.36,
+                    31.07,
+                    23.39,
+                    43.28,
+                    25.48
+                ],
+                barThickness: 48,
+                fill: false,
+                borderRadius: 5,
+                backgroundColor: [
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(186, 34%, 60%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)"
+                ],
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                beginAtZero: true,
+                x: {
+                  grid: {
+                    display: false
+                  }
+                },
+                y: {
+                  grid: {
+                    display: false
+                  }
+                }
+            }
+        }
+    });
