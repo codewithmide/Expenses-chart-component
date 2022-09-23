a9f21aee7307ce9491dc33b1cf9c489114efb4cd
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-23 12:07:24 +0100

    fix

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var ctx = document.getElementById("my_chart").getContext("2d");
                     25.48
                 ],
                 barThickness: 50,
-                fill: false,
+                fill: true,
                 borderRadius: 5,
                 backgroundColor: [
                     "hsl(10, 79%, 65%)",
@@ -38,6 +38,72 @@ var ctx = document.getElementById("my_chart").getContext("2d");
         },
         options: {
             responsive: true,
+            barValueSpacing: 2,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            scales: {
+                beginAtZero: true,
+                x: {
+                  grid: {
+                    display: false
+                  }
+                },
+                y: {
+                  grid: {
+                    display: false
+                  }
+                }
+            }
+        }
+    });
+
+
+// MOBILE VIEW
+
+var ctxm = document.getElementById("mobile_chart").getContext("2d");
+
+    new Chart(ctxm, {
+        type: 'bar',
+        data: {
+            labels: [
+                "mon",
+                "tue",
+                "wed",
+                "thu",
+                "fri",
+                "sat",
+                "sun"
+            ],
+            datasets: [{
+                data: [
+                    17.5,
+                    34.91,
+                    52.36,
+                    31.07,
+                    23.39,
+                    43.28,
+                    25.48
+                ],
+                barThickness: 37,
+                fill: true,
+                borderRadius: 5,
+                backgroundColor: [
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(186, 34%, 60%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)",
+                    "hsl(10, 79%, 65%)"
+                ],
+            }]
+        },
+        options: {
+            responsive: true,
+            barValueSpacing: 2,
             plugins: {
                 legend: {
                     display: false
